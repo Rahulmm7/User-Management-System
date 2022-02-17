892992d098dc0324ec48f3cb70cce17ae30dd7e4
Rahul M M <rahulmanohar007@gmail.com>
2022-02-17 12:58:29 +0530

    path updated

--- routes/admin_route.js
+++ routes/admin_route.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/login', controller.adminLogin);
 router.get('/getAllUsers', auth, controller.getUserData);
 router.put('/updateUserStatus', auth, controller.updateUserStatus);
-router.delete('/userdelete/:id', auth, controller.userdelete);
-router.put('/usersoftdelete', auth, controller.userSoftdelete);
+router.delete('/userDelete/:id', auth, controller.userdelete);
+router.put('/userSoftdelete', auth, controller.userSoftdelete);
 
 module.exports = router;
